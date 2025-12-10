94607f1bbc17f6544f98d02871d3768cb505cda7
Borja10100 <borjatm2004@gmail.com>
2025-12-10 18:25:29 +0100

    init

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import {LandingPage} from './components/pages/landing-page/landing-page';
+
+export const routes: Routes = [
+  {path: '', component: LandingPage}
+];

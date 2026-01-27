e6e5f2890f1a0be485488015309ed123396c28b1
Borja10100 <borjatm2004@gmail.com>
2026-01-27 13:00:57 +0100

    fix(): app.config fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,13 +6,6 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-<<<<<<< HEAD
-import {provideHttpClient} from '@angular/common/http';
-import { registerLocaleData } from '@angular/common';
-import localeEs from '@angular/common/locales/es';
-import { LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
-
-=======
 import {
   HTTP_INTERCEPTORS,
   provideHttpClient,
@@ -23,7 +16,6 @@ import localeEs from '@angular/common/locales/es';
 import { registerLocaleData } from '@angular/common';
 import { LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { AuthInterceptor } from './services/auth-interceptor';
->>>>>>> develop
 
 registerLocaleData(localeEs);
 
@@ -32,17 +24,9 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-<<<<<<< HEAD
-    provideHttpClient(),
-    { provide: LOCALE_ID, useValue: 'es-ES' },
-    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
-    // { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-  ]
-=======
     provideHttpClient(withInterceptorsFromDi()),
     { provide: LOCALE_ID, useValue: 'es-ES' },
     { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
->>>>>>> develop
 };
